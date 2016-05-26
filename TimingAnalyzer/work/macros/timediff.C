4284306e4b28a9f7b2dd64d0e6d2638795787941
Kevin McDermott <kpm82@cornell.edu>
2016-05-26 10:06:05 +0200

    file movements

--- TimingAnalyzer/work/macros/timediff.C
+++
@@ -1,8 +0,0 @@
-void timediff(){
-
-  TFile * file = TFile::Open("../input/tree.root");
-  TTree * tree = (TTree*)file->Get("tree/tree");
-  
-  tree->Draw("el1time-el2time>>h1","nelectrons==2&&zeemass>76.&&zeemass<106.");
-  //  h1->Draw();
-}
