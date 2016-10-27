e2ca284a283d38dbda12821a62947392d08fc205
Kevin McDermott <kpm82@cornell.edu>
2016-10-27 14:21:59 +0200

    add deltaR phi wrapping

--- TimingAnalyzer/plugins/CommonTypes.h
+++ TimingAnalyzer/plugins/CommonTypes.h
@@ -11,6 +11,9 @@
 typedef std::vector<std::pair<DetId,float> > DetIdPairVec;
 typedef std::unordered_map<uint32_t,int> uiiumap;
 
+constexpr float PI    =     3.14159265358979323846;
+constexpr float TWOPI = 2.0*3.14159265358979323846;
+
 inline float rad2  (const float x, const float y){return x*x + y*y;}
 inline float phi   (const float x, const float y){return std::atan2(y,x);}
 inline float theta (const float r, const float z){return std::atan2(r,z);}
@@ -20,5 +23,19 @@ inline float eta   (const float x, const float y, const float z)
 }
 inline float deltaR(const float phi1, const float eta1, const float phi2, const float eta2)
 {
-  return std::sqrt(rad2(eta2-eta1,phi2-phi1));
+  if   (std::abs(phi2-phi1)<PI) 
+  {
+    return std::sqrt(rad2(eta2-eta1,phi2-phi1));
+  }
+  else 
+  {
+    if (phi2-phi1>0.f) 
+    {
+      return std::sqrt(rad2(eta2-eta1,  TWOPI-(phi2-phi1) ));
+    }
+    else // technically, the last sign flip is unnecessary here
+    {
+      return std::sqrt(rad2(eta2-eta1,-(TWOPI+(phi2-phi1))));
+    }
+  }
 }
