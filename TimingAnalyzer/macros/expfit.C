ae90ca50015986bcdc89afb6e165e41e0bbc244f
Kevin McDermott <kpm82@cornell.edu>
2016-11-10 18:34:33 +0100

    include more basic plots of lifetime

--- TimingAnalyzer/macros/expfit.C
+++ TimingAnalyzer/macros/expfit.C
@@ -1,10 +1,13 @@
 void expfit()
 {
-  TFile * file = TFile::Open("output/testdump/cuts_jetpt35.0_phpt100.0_phVIDmedium_rhE1.0_ecalaccept/plots.root");
+  TFile * file = TFile::Open("output/moreplots/cuts_jetpt35.0_phpt100.0_phVIDmedium_rhE1.0_ecalaccept/plots.root");
   TH1F * hist = (TH1F*)file->Get("genN1ctau");
   hist->Scale(1.0/hist->Integral());
+  hist->GetYaxis()->SetTitle("Neutralinos/nNeutralinos");
 
-  TFormula form("texp","[0]*exp(-[1]*x)");
+  TFormula form("texp","[0]*exp(-x/[1])");
   TF1 * fit = new TF1("texp_fit",form.GetName(),0.f,100.f);
+  fit->SetParName(0,"Norm"); fit->SetParameter(0,hist->GetMaximum());
+  fit->SetParName(1,"d");    fit->SetParameter(1,hist->GetMean());
   hist->Fit(fit->GetName());
 }
