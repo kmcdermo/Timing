687f04f154cf1bd94a9578179cbc0e5e38411032
Kevin McDermott <kpm82@cornell.edu>
2017-01-12 19:37:21 +0100

    quick plotter -- still need to compile objets

---
+++ TimingAnalyzer/macros/runQuickPlot.C
@@ -0,0 +1,26 @@
+#include "TString.h"
+#include "TSystem.h"
+#include "TROOT.h"
+
+void setupcpp11() // customize ACLiC's behavior ...
+{
+  TString o;
+  // customize MakeSharedLib
+  o = TString(gSystem->GetMakeSharedLib());
+  o = o.ReplaceAll(" -c ", " -std=c++0x -c ");
+  gSystem->SetMakeSharedLib(o.Data());
+  // customize MakeExe
+  o = TString(gSystem->GetMakeExe());
+  o = o.ReplaceAll(" -c ", " -std=c++0x -c ");
+  gSystem->SetMakeExe(o.Data());
+} 
+
+void runQuickPlot()
+{
+  setupcpp11(); 
+
+  gROOT->LoadMacro("quickPlot.cc++g");
+
+  quickPlot qPlot;
+  qPlot.doPlot();
+}
