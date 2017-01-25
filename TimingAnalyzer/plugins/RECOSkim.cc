c4effc5dcd1ffaf35d361d1f663774e216513c1e
Kevin <kpm82@cornell.edu>
2017-01-25 15:16:35 +0100

    one character fix for HLT

--- TimingAnalyzer/plugins/RECOSkim.cc
+++ TimingAnalyzer/plugins/RECOSkim.cc
@@ -687,7 +687,7 @@ void RECOSkim::endJob() {}
 
 void RECOSkim::beginRun(edm::Run const& iRun, edm::EventSetup const& iSetup) 
 {
-  triggerPathsVector.push_back("HLT_Photon36_R9Id85_OR_CaloId24b0e_Iso50T80L_Photon22_AND_HE10_R9Id65_Eta2_Mass15_v");
+  triggerPathsVector.push_back("HLT_Photon36_R9Id85_OR_CaloId24b40e_Iso50T80L_Photon22_AND_HE10_R9Id65_Eta2_Mass15_v");
 
   HLTConfigProvider hltConfig;
   bool changedConfig = false;
