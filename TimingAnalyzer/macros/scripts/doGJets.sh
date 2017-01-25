2b45d6a4670861fd8228ef4be3731c94978f6114
Kevin <kpm82@cornell.edu>
2017-01-25 02:11:44 +0100

    updates to include standard settings

--- TimingAnalyzer/macros/scripts/doGJets.sh
+++ TimingAnalyzer/macros/scripts/doGJets.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 
-#isEB
-for cut in nocuts cuts
+#ecal accept only
+for cut in cuts
 do
     for HT in 40To100 100To200 200To400 400To600 600ToInf
     do
-	root -l -b -q "runPhotonPlots_HT.C("\"${cut}\",\"GJets\",\"${HT}\",1,0")"
+	root -l -b -q "runPhotonPlots_HT.C("\"${cut}\",\"GJets\",\"${HT}\",0,0")"
     done
 done
 
-#isEE
-for cut in nocuts cuts
-do
-    for HT in 40To100 100To200 200To400 400To600 600ToInf
-    do
-	root -l -b -q "runPhotonPlots_HT.C("\"${cut}\",\"GJets\",\"${HT}\",0,1")"
-    done
-done
+# #isEB
+# for cut in cuts #nocuts cuts
+# do
+#     for HT in 40To100 100To200 200To400 400To600 600ToInf
+#     do
+# 	root -l -b -q "runPhotonPlots_HT.C("\"${cut}\",\"GJets\",\"${HT}\",1,0")"
+#     done
+# done
+
+# #isEE
+# for cut in nocuts cuts
+# do
+#     for HT in 40To100 100To200 200To400 400To600 600ToInf
+#     do
+# 	root -l -b -q "runPhotonPlots_HT.C("\"${cut}\",\"GJets\",\"${HT}\",0,1")"
+#     done
+# done
--- TimingAnalyzer/macros/scripts/doSignal.sh
+++ TimingAnalyzer/macros/scripts/doSignal.sh
@@ -1,25 +1,37 @@
 #!/bin/sh
 
-# isEB
+#ecal accept only
 for reco in withReReco #noReReco
 do
-    for cut in nocuts #cuts
+    for cut in cuts #nocuts #cuts
     do
 	for ctau in 100 2000 6000
 	do
-	    root -l -b -q "runPhotonPlots_signal.C("\"${cut}\",\"${ctau}\",\"${reco}\",1,0")"
+	    root -l -b -q "runPhotonPlots_signal.C("\"${cut}\",\"${ctau}\",\"${reco}\",0,0")"
 	done
     done
 done
 
-# isEE
-for reco in withReReco #noReReco
-do
-    for cut in nocuts #cuts
-    do
-	for ctau in 100 2000 6000
-	do
-	    root -l -b -q "runPhotonPlots_signal.C("\"${cut}\",\"${ctau}\",\"${reco}\",0,1")"
-	done
-    done
-done
+# # isEB
+# for reco in withReReco #noReReco
+# do
+#     for cut in cuts #nocuts #cuts
+#     do
+# 	for ctau in 100 2000 6000
+# 	do
+# 	    root -l -b -q "runPhotonPlots_signal.C("\"${cut}\",\"${ctau}\",\"${reco}\",1,0")"
+# 	done
+#     done
+# done
+
+# # isEE
+# for reco in withReReco #noReReco
+# do
+#     for cut in cuts #nocuts #cuts
+#     do
+# 	for ctau in 100 2000 6000
+# 	do
+# 	    root -l -b -q "runPhotonPlots_signal.C("\"${cut}\",\"${ctau}\",\"${reco}\",0,1")"
+# 	done
+#     done
+# done
