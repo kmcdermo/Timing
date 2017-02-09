ad70b7cca441e425b67c011429ebaf1691255634
Kevin <kpm82@cornell.edu>
2017-02-09 01:51:31 +0100

    hv photon step3

---
+++ GEN_SIM/HVphoton/crab_HVphoton_PAT.py
@@ -0,0 +1,19 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'HVphoton_PAT-v1'
+config.General.workArea    = 'crab_projects'
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName   = 'step3_PAT.py'
+
+config.Data.inputDataset = '/HVphoton_13TeV_GEN_SIM/kmcdermo-HVphoton_RAW2DIGI_L1Reco_RECO-v1-9162a384de1881d24894df196092366e/USER'
+config.Data.inputDBS     = 'phys03'
+config.Data.splitting    = 'FileBased'
+config.Data.unitsPerJob  = 5
+config.Data.publication  = True
+config.Data.publishDBS   = 'phys03'
+config.Data.outputDatasetTag = 'HVphoton_PAT-v1'
+
+config.Site.storageSite = 'T2_CH_CERN'
+
