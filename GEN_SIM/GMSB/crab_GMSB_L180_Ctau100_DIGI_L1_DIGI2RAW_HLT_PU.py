1a716a14a4cb3a51832aee05d2fc55926e72a676
Kevin <kpm82@cornell.edu>
2017-02-28 23:06:48 +0100

    new HLT stuff

--- GEN_SIM/GMSB/crab_GMSB_L180_Ctau100_DIGI_L1_DIGI2RAW_HLT_PU.py
+++ GEN_SIM/GMSB/crab_GMSB_L180_Ctau100_DIGI_L1_DIGI2RAW_HLT_PU.py
@@ -1,19 +1,19 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'GMSB_L180_Ctau6000_newHLT_DIGI_L1_DIGI2RAW_HLT_PU-v1'
+config.General.requestName = 'GMSB_L180_Ctau100_newHLT_DIGI_L1_DIGI2RAW_HLT_PU-v1'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'step1_newHLT_DIGI_L1_DIGI2RAW_HLT_PU.py'
 
-config.Data.inputDataset = '/GMSB_L180_Ctau6000_Pythia8_13TeV_cff_py_GEN_SIM/kmcdermo-GMSB_L180_Ctau6000_Pythia8_13TeV_cff_py_GEN_SIM-bd66f258b1e0722589af2e4f7e9bac46/USER'
+config.Data.inputDataset = '/GMSB_L180_Ctau100_Pythia8_13TeV_cff_py_GEN_SIM_1810/soffi-GMSB_L180_Ctau100_Pythia8_13TeV_cff_py_GEN_SIM_1810-03c68cd33c39b36ecdc91e1e77658ee5/USER'
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.publication = True
 config.Data.publishDBS = 'phys03'
-config.Data.outputDatasetTag = 'GMSB_L180_Ctau6000_newHLT_DIGI_L1_DIGI2RAW_HLT_PU-v1'
+config.Data.outputDatasetTag = 'GMSB_L180_Ctau100_newHLT_DIGI_L1_DIGI2RAW_HLT_PU-v1'
 
 config.Site.storageSite = 'T2_CH_CERN'
 
--- GEN_SIM/GMSB/crab_GMSB_L180_Ctau2000_DIGI_L1_DIGI2RAW_HLT_PU.py
+++ GEN_SIM/GMSB/crab_GMSB_L180_Ctau2000_DIGI_L1_DIGI2RAW_HLT_PU.py
@@ -1,19 +1,19 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'GMSB_L180_Ctau6000_newHLT_DIGI_L1_DIGI2RAW_HLT_PU-v1'
+config.General.requestName = 'GMSB_L180_Ctau2000_newHLT_DIGI_L1_DIGI2RAW_HLT_PU-v1'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'step1_newHLT_DIGI_L1_DIGI2RAW_HLT_PU.py'
 
-config.Data.inputDataset = '/GMSB_L180_Ctau6000_Pythia8_13TeV_cff_py_GEN_SIM/kmcdermo-GMSB_L180_Ctau6000_Pythia8_13TeV_cff_py_GEN_SIM-bd66f258b1e0722589af2e4f7e9bac46/USER'
+config.Data.inputDataset = '/GMSB_L180_Ctau2000_Pythia8_13TeV_cff_py_GEN_SIM_1810/soffi-GMSB_L180_Ctau2000_Pythia8_13TeV_cff_py_GEN_SIM_1810-0164080974557022b1efd00e61df94f7/USER'
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.publication = True
 config.Data.publishDBS = 'phys03'
-config.Data.outputDatasetTag = 'GMSB_L180_Ctau6000_newHLT_DIGI_L1_DIGI2RAW_HLT_PU-v1'
+config.Data.outputDatasetTag = 'GMSB_L180_Ctau2000_newHLT_DIGI_L1_DIGI2RAW_HLT_PU-v1'
 
 config.Site.storageSite = 'T2_CH_CERN'
 
