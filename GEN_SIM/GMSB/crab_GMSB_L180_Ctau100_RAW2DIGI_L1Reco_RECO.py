52e90d86850f2bd841079ef1d17187e091b71500
Kevin <kpm82@cornell.edu>
2017-03-01 10:25:24 +0100

    next round of new hlt

--- GEN_SIM/GMSB/crab_GMSB_L180_Ctau100_RAW2DIGI_L1Reco_RECO.py
+++ GEN_SIM/GMSB/crab_GMSB_L180_Ctau100_RAW2DIGI_L1Reco_RECO.py
@@ -7,7 +7,7 @@ config.General.workArea = 'crab_projects'
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'step2_customise_RAW2DIGI_L1Reco_RECO.py'
 
-config.Data.inputDataset = '/GMSB_L180_Ctau6000_Pythia8_13TeV_cff_py_GEN_SIM/kmcdermo-GMSB_L180_Ctau6000_newHLT_DIGI_L1_DIGI2RAW_HLT_PU-v1-ba0862a5ff778aabb2ae124a2b73a560/USER'
+config.Data.inputDataset = '/GMSB_L180_Ctau100_Pythia8_13TeV_cff_py_GEN_SIM_1810/kmcdermo-GMSB_L180_Ctau100_newHLT_DIGI_L1_DIGI2RAW_HLT_PU-v1-ba0862a5ff778aabb2ae124a2b73a560/USER'
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 5
