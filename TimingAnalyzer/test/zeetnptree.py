7a954d65ffa9a2a761a39376fcebd7c6afb05fe8
Kevin McDermott <kpm82@cornell.edu>
2017-03-02 21:58:17 +0100

    first attempt with Zee TnP tree

---
+++ TimingAnalyzer/test/zeetnptree.py
@@ -0,0 +1,121 @@
+import os, re
+import FWCore.ParameterSet.Config as cms
+  
+### CMSSW command line parameter parser
+from FWCore.ParameterSet.VarParsing import VarParsing
+options = VarParsing ('python')
+
+## processName
+options.register (
+	'processName','TREE',VarParsing.multiplicity.singleton,VarParsing.varType.string,
+	'process name to be considered');
+
+options.register (
+	'miniAODProcess','PAT',VarParsing.multiplicity.singleton,VarParsing.varType.string, #why RECO>???
+	'process name used for miniAOD production (target is miniAODv2)');
+
+## outputFile Name
+options.register (
+	'outputFileName','zeetnptree.root',VarParsing.multiplicity.singleton,VarParsing.varType.string,
+	'output file name created by cmsRun');
+
+## GT to be used    
+options.register (
+	'globalTag','80X_dataRun2_2016SeptRepro_v7',VarParsing.multiplicity.singleton,VarParsing.varType.string,
+	'gloabl tag to be used');
+
+## nThreads to run
+options.register (
+	'nThreads',1,VarParsing.multiplicity.singleton, VarParsing.varType.int,
+	'default number of threads');
+
+## do a demo run over only 100k events
+options.register (
+	'demoMode',False,VarParsing.multiplicity.singleton,VarParsing.varType.bool,
+	'flag to run over only 100k events as a demo');
+
+## parsing command line arguments
+options.parseArguments()
+
+print "##### Settings ######"
+print "Running with processName         = ",options.processName	
+print "Running with miniAODProcess      = ",options.miniAODProcess	
+print "Running with outputFileName      = ",options.outputFileName	
+print "Running with globalTag           = ",options.globalTag	
+print "Running with nThreads            = ",options.nThreads
+print "Running with demoMode            = ",options.demoMode
+print "#####################"
+
+## Define the CMSSW process
+process = cms.Process(options.processName)
+
+## Load the standard set of configuration modules
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+## Message Logger settings
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.destinations = ['cout', 'cerr']
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+## Define the input source
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring( 
+		# run 2016B
+		'/store/data/Run2016B/DoubleEG/MINIAOD/23Sep2016-v3/00000/0060C751-C097-E611-9FE6-FA163EFD4308.root'
+		) 
+)
+
+## Set the process options -- Display summary at the end, enable unscheduled execution
+if options.nThreads == 1 or options.nThreads == 0:
+	process.options = cms.untracked.PSet( 
+		allowUnscheduled = cms.untracked.bool(True),
+		wantSummary = cms.untracked.bool(True))
+else:
+	process.options = cms.untracked.PSet( 
+		allowUnscheduled = cms.untracked.bool(True),
+		wantSummary = cms.untracked.bool(True),
+		numberOfThreads = cms.untracked.uint32(options.nThreads),
+		numberOfStreams = cms.untracked.uint32(options.nThreads))
+
+## How many events to process
+if not options.demoMode:
+	process.maxEvents = cms.untracked.PSet( 
+		input = cms.untracked.int32(options.maxEvents))
+else:
+	process.maxEvents = cms.untracked.PSet( 
+		input = cms.untracked.int32(10000))
+
+# Set the global tag depending on the sample type
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag.globaltag = options.globalTag  
+
+# run cut-based electron ID https://twiki.cern.ch/twiki/bin/viewauth/CMS/CutBasedElectronIdentificationRun2
+from Timing.TimingAnalyzer.ElectronTools_cff import ElectronTools
+ElectronTools(process,options.isMC)
+
+## Create output file
+## Setup the service to make a ROOT TTree
+process.TFileService = cms.Service("TFileService", 
+		                   fileName = cms.string(options.outputFileName))
+
+# Make the tree 
+process.tree = cms.EDAnalyzer("ZeeTnPTree",
+   ## trigger info + apply filter?
+   triggerResults = cms.InputTag("TriggerResults", "", "HLT"),
+   ## vertexes			    	
+   vertices  = cms.InputTag("offlineSlimmedPrimaryVertices"),
+   ## electrons
+   vetoElectronID   = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Summer16-80X-V1-veto"),
+   looseElectronID  = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Summer16-80X-V1-loose"),
+   mediumElectronID = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Summer16-80X-V1-medium"),
+   tightElectronID  = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Summer16-80X-V1-tight"),
+   electrons        = cms.InputTag("calibratedElectrons"),
+   ## ecal recHits			      
+   recHitsEB = cms.InputTag("reducedEgamma", "reducedEBRecHits"),
+   recHitsEE = cms.InputTag("reducedEgamma", "reducedEERecHits"),
+)
+
+# Set up the path
+process.treePath = cms.Path(process.selectedElectrons * process.calibratedElectrons * process.egmGsfElectronIDs * process.tree)
