a54c6bc377374da17e856cac9efab3163c710237
Kevin McDermott <kpm82@cornell.edu>
2017-07-13 11:32:03 +0200

    fix cm to mm, fix other small bugs for autogen HVDS configs

--- GEN_SIM/Configuration/GenProduction/python/ThirteenTeV/HVDS/autogenHVDScsv.sh
+++ GEN_SIM/Configuration/GenProduction/python/ThirteenTeV/HVDS/autogenHVDScsv.sh
@@ -4,9 +4,9 @@ for mzp in 300 500 800 1000
 do
     for mdp in 20 40 60
     do
-	for ctau in 10 50 100 250 500 1000
+	for ctau in 1 100 500 1000 2500 5000 10000
 	do
-	    echo "HVDS_MZp-${mzp}_MDP-${mdp}_Ctau-${ctau}_13TeV-pythia8 100000 https://github.com/kmcdermo/Timing/blob/9XY/GEN_SIM/Configuration/GenProduction/python/ThirteenTeV/HVDS/HVDS_MZP${mzp}_MDP${mdp}_Ctau${ctau}_Pythia8_13TeV_cff.py Pythia8" >> hvds.csv
+	    echo "HVDS_MZp-${mzp}_MDP-${mdp}_Ctau-${ctau}mm_13TeV-pythia8,100000,https://github.com/kmcdermo/Timing/blob/9XY/GEN_SIM/Configuration/GenProduction/python/ThirteenTeV/HVDS/HVDS_MZP${mzp}_MDP${mdp}_Ctau${ctau}mm_Pythia8_13TeV_cff.py,Pythia8" >> hvds.csv
 	done
     done
 done
