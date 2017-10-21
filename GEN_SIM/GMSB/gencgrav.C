730dd79994ca0c4688144e85f26d7fcd79588b5f
Kevin McDermott <kpm82@cornell.edu>
2017-10-21 15:22:44 +0200

    comment for old points

--- GEN_SIM/GMSB/gencgrav.C
+++ GEN_SIM/GMSB/gencgrav.C
@@ -8,9 +8,18 @@ inline float getcgrav(const int lamb, const float ctau)
 
 void gencgrav()
 {
-  std::vector<float> ctaus = {0.001,0.01,0.1,1,5,10,25,50,100,200,300,500,750,1000,1500,2000,2500,3000}; // cm
-  std::vector<int>   lambs = {50,100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500}; // tev
+  // new points for tuning
+  //  std::vector<float> ctaus = {0.001,0.01,0.1,1,5,10,25,50,100,200,300,500,750,1000,1500,2000,2500,3000}; // cm
+  //  std::vector<int>   lambs = {50,100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500}; // tev
 
+  // points for livia
+  std::vector<float> ctaus = {0.1,3,6,15,30,200,300,500,1000,1500};
+  std::vector<int>   lambs = {100,150,200,250,300,350,400,450,500};
+
+  // submitted points
+  //  std::vector<float> ctaus = {0.1,10,6,200,400,600,800,1000,1200};
+  //  std::vector<int>   lambs = {100,150,200,250,300,350,400};
+  
   TString outdir = "Lambda";
   FileStat_t dummyFileStat;
   if (gSystem->GetPathInfo(outdir.Data(), dummyFileStat) == 1)
