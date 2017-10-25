ff76ea698fc9d6734f80914555946776b11f1187
Kevin McDermott <kpm82@cornell.edu>
2017-10-25 17:48:50 +0200

    bug fix for script

--- TimingAnalyzer/dispho_work/scripts/copy-eaplots.sh
+++ TimingAnalyzer/dispho_work/scripts/copy-eaplots.sh
@@ -6,7 +6,7 @@ outdir=${2:-"eff_areas"}
 mkdir -p ${outdir}
 
 declare -a arr=("phoecaliso_EB_OOT_v_nvtx_quant_0.90"
-                "phoecaliso_EB_OOT_v_nvtx_quant_0.90"
+                "phohcaliso_EB_OOT_v_nvtx_quant_0.90"
 		"photrkiso_EB_OOT_v_nvtx_quant_0.90")
 for i in "${arr[@]}"
 do
