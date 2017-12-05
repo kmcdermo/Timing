63a24caa15101af8e3c1dbde78e24066d90ef892
Kevin <kpm82@cornell.edu>
2017-12-05 19:00:23 +0100

    remove extra bits

--- TimingAnalyzer/dispho_work/macros/scripts/skimAndMerge/skimAndMerge_SignalGridPoint.sh
+++ TimingAnalyzer/dispho_work/macros/scripts/skimAndMerge/skimAndMerge_SignalGridPoint.sh
@@ -6,13 +6,14 @@ source scripts/common_variables.sh
 label=${1}
 model=${2}
 gridpoint=${3}
+extra=${4:-""}
 
 ## global vars
 mcbase="${model}"
 text="${mcbase}_${gridpoint}"
 
 ## directories needed
-indir="${eosbase}/${mcbase}_${gridpoint}/${label}_${mcbase}_${gridpoint}"
+indir="${eosbase}/${mcbase}_${gridpoint}${extra}/${label}_${mcbase}_${gridpoint}${extra}"
 tmpdir="${tmpbase}/${mcbase}/${gridpoint}"
 outdir="${outbase}/MC/${mcbase}/${gridpoint}"
 
--- TimingAnalyzer/dispho_work/macros/scripts/skimAndMerge/skimAndMerge_SignalLoop.sh
+++ TimingAnalyzer/dispho_work/macros/scripts/skimAndMerge/skimAndMerge_SignalLoop.sh
@@ -4,10 +4,11 @@ source scripts/common_variables.sh
 
 ## input
 label=${1}
+extra=${2:-""}
 
-for sample in "GMSB L200TeV_CTau400cm_930"
+for sample in "GMSB L200TeV_CTau400cm"
 do echo ${sample} | while read -r model gridpoint
     do
-	./scripts/skimAndMerge/skimAndMerge_SignalGridPoint.sh ${label} ${model} ${gridpoint}
+	./scripts/skimAndMerge/skimAndMerge_SignalGridPoint.sh ${label} ${model} ${gridpoint} ${extra}
     done
 done
