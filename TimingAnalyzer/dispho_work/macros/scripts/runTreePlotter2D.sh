9f766a3fb4e6787185b6afdac3f96b43dbcb26d7
Kevin <kpm82@cornell.edu>
2018-01-17 00:35:24 +0100

    asdf

--- TimingAnalyzer/dispho_work/macros/scripts/runTreePlotter2D.sh
+++ TimingAnalyzer/dispho_work/macros/scripts/runTreePlotter2D.sh
@@ -4,10 +4,10 @@ basecut="phoisEB_0&&phopt_0>70"
 gjetscut="${basecut}&&njets<=2&&((phogedID_0==3&&!phoisOOT_0)||(phoootID_0==3&&phoisOOT_0))&&abs(TVector2::Phi_mpi_pi(jetphi_0-phophi_0))>2.1&&(jetpt_0/phopt_0>0.6)&&(jetpt_0/phopt_0<1.4)&&phor9_0>0.95&&hltPho50"
 qcdcut="${basecut}&&njets>2&&((phogedID_0==1&&!phoisOOT_0)||(phoootID_0==1&&phoisOOT_0))&&hltPho50"
 
-commoncut=${1:-"${gjetscut}"}
+commoncut=${1:-"${qcdcut}"}
 text=${2:-"met_vs_time"}
 inconfig=${3:-"plot_config/met_vs_time.txt"}
 
 root -l -b -q runTreePlotter2D.C\(\"${commoncut}\",\"${text}\",\"${inconfig}\"\)
 
-cp ${text}.png  ~/www/quick/
+cp ${text}.root ~/www/quick/
