2bb9765e4180f3fb4c33ef1f4ec284c43c2f7969
Kevin <kpm82@cornell.edu>
2018-01-17 00:17:50 +0100

    update id

--- TimingAnalyzer/dispho_work/macros/scripts/runTreePlotter2D.sh
+++ TimingAnalyzer/dispho_work/macros/scripts/runTreePlotter2D.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 basecut="phoisEB_0&&phopt_0>70"
-gjetscut="${basecut}&&njets<=2&&phoID_0==3&&abs(TVector2::Phi_mpi_pi(jetphi_0-phophi_0))>2.1&&(jetpt_0/phopt_0>0.6)&&(jetpt_0/phopt_0<1.4)&&phor9_0>0.95&&hltPho50"
-qcdcut="${basecut}&&njets>2&&((phoID_0==1&&!phoisOOT_0)||(phoID_0==0&&phoisOOT_0))&&hltPho50"
+gjetscut="${basecut}&&njets<=2&&((phogedID_0==3&&!phoisOOT_0)||(phoootID_0==3&&phoisOOT_0))&&abs(TVector2::Phi_mpi_pi(jetphi_0-phophi_0))>2.1&&(jetpt_0/phopt_0>0.6)&&(jetpt_0/phopt_0<1.4)&&phor9_0>0.95&&hltPho50"
+qcdcut="${basecut}&&njets>2&&((phogedID_0==1&&!phoisOOT_0)||(phoootID_0==1&&phoisOOT_0))&&hltPho50"
 
 commoncut=${1:-"${gjetscut}"}
 text=${2:-"met_vs_time"}
