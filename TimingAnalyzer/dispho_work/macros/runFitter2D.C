e5671f3637b4f42d3834918c689fe4af1c71cd97
Kevin <kpm82@cornell.edu>
2018-01-17 21:00:35 +0100

    rename correctly fitter

--- TimingAnalyzer/dispho_work/macros/runFitter2D.C
+++ TimingAnalyzer/dispho_work/macros/runFitter2D.C
@@ -2,7 +2,7 @@
 #include "common/Common.cpp+"
 #include "Fitter2D.cpp+"
 
-void runFit(const TString & infilename, const TString & outfilename)
+void runFitter2D(const TString & infilename, const TString & outfilename)
 {
   Fitter2D fitter(infilename,outfilename);
   fitter.MakeFit();
