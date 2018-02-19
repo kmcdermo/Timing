0ff67376eaeafbfb8dee2e83f75e1adc1991a81f
Kevin <kpm82@cornell.edu>
2018-02-19 22:58:24 +0100

    add control plot config

--- TimingAnalyzer/dispho_work/macros/scripts/computePUWeights.sh
+++ TimingAnalyzer/dispho_work/macros/scripts/computePUWeights.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
 ## first run plotter
-var=${1:-"nvtx"}
-commoncut=${2:-"(phopt_0>70.0&&phoID_0>=2)&&hltSignal"}
-text=${3:-"puweights"}
-nbinsx=${4:-120}
-xlow=${5:-0}
-xhigh=${6:-120}
-islogx=${7:-0}
-islogy=${8:-1}
-title=${9:-${var}}
-xtitle=${10:-${var}}
-ytitle=${11:-"Events"}
+cutconfig=${1:-"cut_config/signal_deg.txt"}
+plotconfig=${2:-"plot_config/nvtx.txt"}
+outfiletext=${3:-"puweights"}
 
-root -l -b -q runTreePlotter.C\(\"${var}\",\"${commoncut}\",\"${text}\",${nbinsx},${xlow},${xhigh},${islogx},${islogy},\"${title}\",\"${xtitle}\",\"${ytitle}\"\)
+root -l -b -q runTreePlotter.C\(\"${cutconfig}\",\"${plotconfig}\",\"${outfiletext}\"\)
 
 ## then run pu computation
 
-root -l -b -q computePUWeights.C\(\"${text}\"\)
+root -l -b -q computePUWeights.C\(\"${outfiletext}\"\)
 
 ## move it somewhere SAFE
 
-xrdcp ${text}.root root://eoscms//store/user/kmcdermo/nTuples/
+#xrdcp ${text}.root root://eoscms//store/user/kmcdermo/nTuples/
--- TimingAnalyzer/dispho_work/macros/scripts/controlplots.sh
+++ TimingAnalyzer/dispho_work/macros/scripts/controlplots.sh
@@ -1,37 +1,9 @@
 #!/bin/bash
 
-## define cuts
-basecut="phoisEB_0&&phopt_0>70"
-
-gjetscut="gjets_ctrl ${basecut}&&njets<=2&&phoID_0==3&&abs(TVector2::Phi_mpi_pi(jetphi_0-phophi_0))>2.1&&(jetpt_0/phopt_0>0.6)&&(jetpt_0/phopt_0<1.4)&&phor9_0>0.95&&hltPho50"
-qcdcut="qcd_ctrl ${basecut}&&njets>2&&((phoID_0==1&&!phoisOOT_0)||(phoID_0==0&&phoisOOT_0))&&hltPho50"
-
-## define plots
-logaxis="0 1" # !islogx && !islogy
-ytitle="Events"
-delim="XXX"
-
-## blank spaces in titles use XXX
-phopt_0="phopt_0 phopt_0 100 0 1000 ${logaxis} phopt_0 LeadingXXXPhotonXXXp_{T}XXX[GeV/c] ${ytitle} ${delim}"
-phoeta_0="phoeta_0 phoeta_0 30 -1.5 1.5 ${logaxis} phoeta_0 LeadingXXXPhotonXXX#eta ${ytitle} ${delim}"
-phoseedtime_0="rhtime[phoseed_0] phoseedtime_0 60 -5 25 ${logaxis} phoseedtime_0 LeadingXXXPhotonXXXSeedXXXTimeXXX[ns] ${ytitle} ${delim}"
-met="t1pfMETpt met 100 0 300 ${logaxis} met METXXX[GeV] ${ytitle} ${delim}"
-
-## loop over cuts
-for plot in phopt_0 phoeta_0 phoseedtime_0 met
-do echo ${!plot} | while read -r var vartext plotinfo
+for sel in  qcd
+do
+    for plot in phopt_0 phoeta_0 phophi_0 phoseedtime_0 met njetseta3 jetHTeta3
     do
-	for cut in qcdcut gjetscut
-	do echo ${!cut} | while read -r text commoncut
-	    do 
-		if [ "${vartext}" == "phoseedtime_0" ] ; 
-		then 
-		    ./scripts/runTreePlotter.sh ${var} "${commoncut}&&phoseed_0>=0" ${text}_${vartext} ${plotinfo}
-		else
-		    ./scripts/runTreePlotter.sh ${var} ${commoncut} ${text}_${vartext} ${plotinfo}
-		fi
-	    done
-	done
+	./scripts/runTreePlotter.sh "cut_config/control_${sel}.txt" "plot_config/${plot}.txt" ${plot} "plots/control_${sel}"
     done
 done
-
