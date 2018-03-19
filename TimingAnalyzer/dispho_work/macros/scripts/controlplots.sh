db375c00bba398b963c705ec2ef5cbf48b46401d
Kevin <kpm82@cornell.edu>
2018-03-19 17:11:53 +0100

    add plots

--- TimingAnalyzer/dispho_work/macros/scripts/controlplots.sh
+++ TimingAnalyzer/dispho_work/macros/scripts/controlplots.sh
@@ -2,7 +2,7 @@
 
 for sel in gjets
 do
-    for plot in phopt_0 phoeta_0 phophi_0 phoseedtime_0 met njetsidL jetHTidL phopt_1
+    for plot in phopt_0 phoeta_0 phophi_0 phoseedtime_0 met met_slim njetsidL jetHTidL phopt_1
     do
 	./scripts/runTreePlotter.sh "cut_config/control_${sel}.txt" "plot_config/${plot}.txt" ${plot} "plots/${sel}_wpixveto"
     done
