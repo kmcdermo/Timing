32bd1ae81a7d47c8b914c0b7cb806815384eae8c
Kevin <kpm82@cornell.edu>
2018-03-20 04:33:45 +0100

    tmp updates

--- TimingAnalyzer/dispho_work/macros/scripts/controlplots.sh
+++ TimingAnalyzer/dispho_work/macros/scripts/controlplots.sh
@@ -2,8 +2,8 @@
 
 for sel in gjets
 do
-    for plot in phopt_0 phoeta_0 phophi_0 phoseedtime_0 met met_slim njetsidL jetHTidL phopt_1
+    for plot in njetsidL jetHTidL njetsidT jetHTidT phopt_0 phoeta_0 phophi_0 phoseedtime_0 met met_slim phopt_1
     do
-	./scripts/runTreePlotter.sh "cut_config/control_${sel}.txt" "plot_config/${plot}.txt" ${plot} "plots/${sel}_wpixveto"
+	./scripts/runTreePlotter.sh "cut_config/control_${sel}.txt" "plot_config/${plot}.txt" ${plot} "plots/${sel}_wpixveto_noidpho1_jetpt0gt50"
     done
 done
