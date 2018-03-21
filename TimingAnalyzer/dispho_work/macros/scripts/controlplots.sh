55f6ed5491a1755028061f470e06b56a308efc3d
Kevin <kpm82@cornell.edu>
2018-03-21 03:06:09 +0100

    update control plots script

--- TimingAnalyzer/dispho_work/macros/scripts/controlplots.sh
+++ TimingAnalyzer/dispho_work/macros/scripts/controlplots.sh
@@ -2,7 +2,7 @@
 
 sel=${1:-"control_gjets"}
 
-for plot in phopt_0 phoeta_0 phophi_0 phoseedtime_0 phosieie_0 phoHoE_0 phosmaj_0 phosmin_0 phoEcalPFClIso_0 phoHcalPFClIso_0 phoTrkIso_0 phopt_1 diphomass met met_slim njetsidL jetHTidL njetsidT jetHTidT
+for plot in phopt_0 phoeta_0 phophi_0 phoseedtime_0 phosieie_0 phoHoE_0 phosmaj_0 phosmin_0 phoEcalPFClIso_0 phoHcalPFClIso_0 phoTrkIso_0 phopt_1 diphomass delphi njetsidL jetHTidL njetsidT jetHTidT met met_slim
 do
     ./scripts/runTreePlotter.sh "cut_config/${sel}.txt" "plot_config/${plot}.txt" "${plot}_${sel}" "plots/full/${sel}"
 done
