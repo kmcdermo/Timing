7b1a679ce5c02670d3e2fb5389b58a2b2e98654c
Kevin <kpm82@cornell.edu>
2018-03-23 15:56:05 +0100

    prelim

---
+++ TimingAnalyzer/dispho_work/macros/scripts/checkProcessed.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+samples=${1}
+files=${2}
+
+
+
+./run
--- TimingAnalyzer/dispho_work/macros/scripts/skimAndMerge/processSkimAndMerge.sh
+++ TimingAnalyzer/dispho_work/macros/scripts/skimAndMerge/processSkimAndMerge.sh
@@ -34,30 +34,5 @@ mkdir -p ${tmpdir}
 ## produce sum of weights
 echo "Getting sum of weights"
 ./scripts/runSumWeights.sh ${rootbase}/${eosdir} ${files} ${wgtfile}
-sumwgts=$(grep "Sum of weights: " ${wgtfile} | cut -d " " -f 4)
 
-## read in each file and skim
-echo "Running skimming macro"
-nfiles=$(wc -l ${files})
-counter="1"
-while IFS='' read -r line || [[ -n "${line}" ]]; do
-    echo "Working on file" ${counter} "out of" ${nfiles} "[filename: ${line}]"
-    ./scripts/runSkimmer.sh ${rootbase}/${eosdir} ${tmpdir} ${line} ${sumwgts} ${redophoid}
-    counter=$((${counter} + 1))
-done < "${files}"
-
-## remove txt files
-echo "Removing txt files"
-rm ${wgtfile}
 rm ${files}
-
-## Hadd on tmp 
-echo "Hadding skims on tmp and then removing individual skim files"
-hadd -O -k ${tmpdir}/${outfile} ${tmpdir}/${infiles}
-rm -rf ${tmpdir}/${infiles}
-
-## Copy back to EOS
-echo "Copy hadded skim to EOS"
-eos mkdir -p ${outdir}
-xrdcp -r ${tmpdir}/${outfile} ${rootbase}/${outdir}
-rm -rf ${tmpdir}/${outfile}
