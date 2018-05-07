7e6d1b4eb883a01285c1518c12b52b912e71d671
Kevin <kpm82@cornell.edu>
2018-05-07 18:12:31 +0200

    update xsec

--- GEN_SIM/test/xsec/getXSecFromLogs.sh
+++ GEN_SIM/test/xsec/getXSecFromLogs.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 
 ## SETUP
-basename=GMSB_L200TeV_CTau400cm_930
-path=/store/user/kmcdermo/${basename}/${basename}_step0
+basename=GMSB_L250TeV_CTau0p1cm_step0
+path=/eos/cms/store/group/phys_egamma/soffi/displacedPhotons/GEN-SIM_2304218/${basename}/${basename}
 output=${basename}_xsecs.txt
 
-## GET LOGS FROM EOS
-timestamp=$(eos ls ${path})
-xrdcp -r root://eoscms/${path}/${timestamp}/0000/log . 
+echo "Copying log tar files"
 
-## HOW MANY?
-njobs=$( ls cmsRun_*.log.tar.gz | wc -l)
+## GET LOGS FROM EOS
+timestamp=$(ls ${path})
+cp ${path}/${timestamp}/0000/log/cmsRun_*.log.tar.gz .
+
+echo "Extracting xsecs..."
 
 ## EXTRACT XSEC FROM LOGS
-for i in `seq 1 ${njobs}`
+for file in cmsRun_*.log.tar.gz
 do
-    tar -zxvf cmsRun_${i}.log.tar.gz
-    rm -rf cmsRun_${i}.log.tar.gz
+    i=$(echo ${file} | sed 's/.*cmsRun_\(.*\)\.log\.tar\.gz/\1/')
+
+    echo "Untarring" ${file}
+    tar -zxf ${file}
+    rm -rf ${file}
     rm cmsRun-stderr-${i}.log
     rm FrameworkJobReport-${i}.xml
 
+    echo "Grepping for xsec"
     xsec=$( grep "final cross section" cmsRun-stdout-${i}.log | cut -d " " -f 7 )
     exsec=$( grep "final cross section" cmsRun-stdout-${i}.log | cut -d " " -f 9 )
-    
     echo ${xsec} ${exsec} >> ${output}
 
     rm cmsRun-stdout-${i}.log
 done
 
 ## COMPUTE AVERAGE XSEC AND DUMP
+echo "Computing total xsec"
 root -l -b -q computeAverageXSec.C\(\"${output}\"\)
 
 ## CLEANUP
