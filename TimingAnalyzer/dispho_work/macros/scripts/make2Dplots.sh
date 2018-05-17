7d55c40489e57442963f6fa6f8ba0e68811c2b56
Kevin McDermott <kpm82@cornell.edu>
2018-05-16 20:24:00 -0400

    update 2d plot maker script

--- TimingAnalyzer/dispho_work/macros/scripts/make2Dplots.sh
+++ TimingAnalyzer/dispho_work/macros/scripts/make2Dplots.sh
@@ -3,10 +3,11 @@
 outdir=${1:-"plots"}
 plot="met_vs_time"
 insel="always_true"
+misc="empty.txt"
 
-for group in "qcd_skim control_qcd SinglePhoton" "gjets_skim control_gjets_deg DoubleEG" "sr_skim signal_idT_hlt_nJet3pt30_ht400jetpt30_sumEtjetpt30phopt0_hltreco SinglePhoton"
-do echo ${group} | while read -r infile sel pdname
+for group in "qcd signals_qcd control_qcd SinglePhoton" "gjets signals_gjets control_gjets_deg DoubleEG" "sr signals_sr signal_idT_hlt_nJet3pt30_ht400jetpt30_sumEtjetpt30phopt0_hltreco SinglePhoton"
+do echo ${group} | while read -r infile insignalfile sel pdname
     do
-	./scripts/runTreePlotter2D.sh "skims/${infile}.root" "cut_config/${insel}.txt" "plot_config/${plot}.txt" "${plot}_${sel}_${pdname}" "${outdir}/${sel}"
+	./scripts/runTreePlotter2D.sh "skims/${infile}.root" "skims/${insignalfile}.root" "cut_config/${insel}.txt" "plot_config/${plot}.txt" "plot_config/${misc}.txt" "${plot}_${sel}_${pdname}" "${outdir}/${sel}"
     done
 done
