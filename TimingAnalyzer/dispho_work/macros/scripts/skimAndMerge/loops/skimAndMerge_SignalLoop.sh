6a97483dc2d6cc5a9402a6b54b6ee76b3b527fb1
Kevin <kpm82@cornell.edu>
2018-05-25 23:26:09 +0200

    update to loop for signals since quick

--- TimingAnalyzer/dispho_work/macros/scripts/skimAndMerge/loops/skimAndMerge_SignalLoop.sh
+++ TimingAnalyzer/dispho_work/macros/scripts/skimAndMerge/loops/skimAndMerge_SignalLoop.sh
@@ -4,11 +4,14 @@ source scripts/common_variables.sh
 
 ## input
 label=${1}
-extra=${2:-""}
 
-for sample in "GMSB L200TeV_CTau400cm"
-do echo ${sample} | while read -r model gridpoint
+for ctau in 0p1 10
+do
+    for lambda in 100 150 200 250 300 350 400
     do
-	./scripts/skimAndMerge/skimAndMerge_SignalGridPoint.sh ${label} ${model} ${gridpoint} ${extra}
+	gridpoint="L${lambda}TeV_CTau${ctau}cm"
+	./scripts/skimAndMerge/skimAndMerge_SignalGridPoint.sh ${label} "GMSB" ${gridpoint} "_step0"
     done
 done
+
+./scripts/skimAndMerge/skimAndMerge_SignalGridPoint.sh ${label} "GMSB" "L200TeV_CTau400cm" "_930"
