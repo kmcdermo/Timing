933a46c7aebe4d47d1b58c1b889f39dccf37e2bc
Kevin <kpm82@cornell.edu>
2018-05-30 18:54:44 +0200

    update with new signals

--- TimingAnalyzer/test/multicrab_dispho_Signal.py
+++ TimingAnalyzer/test/multicrab_dispho_Signal.py
@@ -95,18 +95,38 @@ def main():
             
             ['/GMSB_L100TeV_CTau0p1cm_step0/soffi-GMSB_L100TeV_CTau0p1cm_step3-401494f1e1638aa51280a90edc94ebc0/USER', '2.18318', '1', '0.9443385', 'isGMSB'],
             ['/GMSB_L100TeV_CTau10cm_step0/soffi-GMSB_L100TeV_CTau10cm_step3-7b4951d75b0cdfce8b97c445b952682b/USER', '2.19101', '1', '0.9443385', 'isGMSB'],
+            ['/GMSB_L100TeV_CTau600cm_step0/soffi-GMSB_L100TeV_CTau600cm_step3-7b4951d75b0cdfce8b97c445b952682b/USER', '2.1437', '1', '0.944338560', 'isGMSB'],
+            ['/GMSB_L100TeV_CTau1200cm_step0/soffi-GMSB_L100TeV_CTau1200cm_step3-dbd969891b3cd45cc603b8e4d5285af1/USER', '2.10999', '1', '0.944338381', 'isGMSB'],
+
             ['/GMSB_L150TeV_CTau0p1cm_step0/soffi-GMSB_L150TeV_CTau0p1cm_step3-401494f1e1638aa51280a90edc94ebc0/USER', '0.217511', '1', '0.857347369', 'isGMSB'],
             ['/GMSB_L150TeV_CTau10cm_step0/soffi-GMSB_L150TeV_CTau10cm_step3-5febe9b5e64145211cc865f9339571bf/USER', '0.21779', '1', '0.857347429', 'isGMSB'],
+            ['/GMSB_L150TeV_CTau600cm_step0/soffi-GMSB_L150TeV_CTau600cm_step3-5febe9b5e64145211cc865f9339571bf/USER', '0.21713', '1', '0.857347429', 'isGMSB'],
+            ['/GMSB_L150TeV_CTau1200cm_step0/soffi-GMSB_L150TeV_CTau1200cm_step3-85cad58e5cafa48fcf4b651c13572891/USER', '0.217984', '1', '0.857347369', 'isGMSB'],
+
             ['/GMSB_L200TeV_CTau0p1cm_step0/soffi-GMSB_L200TeV_CTau0p1cm_step3-401494f1e1638aa51280a90edc94ebc0/USER', '0.0401172', '1', '0.814181864', 'isGMSB'],
             ['/GMSB_L200TeV_CTau10cm_step0/soffi-GMSB_L200TeV_CTau10cm_step3-401494f1e1638aa51280a90edc94ebc0/USER', '0.0400735', '1', '0.814181864', 'isGMSB'],
+            ['/GMSB_L200TeV_CTau600cm_step0/soffi-GMSB_L200TeV_CTau600cm_step3-401494f1e1638aa51280a90edc94ebc0/USER', '0.0400124', '1', '0.814181924', 'isGMSB'],
+            ['/GMSB_L200TeV_CTau1200cm_step0/soffi-GMSB_L200TeV_CTau1200cm_step3-5b7c60e1fdaf50c5a7b959ba90f832d2/USER', '0.0403603', '1', '0.814181864', 'isGMSB'],
+
             ['/GMSB_L250TeV_CTau0p1cm_step0/soffi-GMSB_L250TeV_CTau0p1cm_step3-7b4951d75b0cdfce8b97c445b952682b/USER', '0.0109216', '1', '0.792751014', 'isGMSB'],
             ['/GMSB_L250TeV_CTau10cm_step0/soffi-GMSB_L250TeV_CTau10cm_step3-54655a0391212dbba2984afe28e21bc3/USER', '0.0109236', '1', '0.792751014', 'isGMSB'],
+            ['/GMSB_L250TeV_CTau600cm_step0/soffi-GMSB_L250TeV_CTau600cm_step3-54655a0391212dbba2984afe28e21bc3/USER', '0.0109279', '1', '0.792751074', 'isGMSB'],
+            ['/GMSB_L250TeV_CTau1200cm_step0/soffi-GMSB_L250TeV_CTau1200cm_step3-353c732828f8f1a8f78f268049f7b0ed/USER', '0.0109101', '1', '0.792751014', 'isGMSB'],
+
             ['/GMSB_L300TeV_CTau0p1cm_step0/soffi-GMSB_L300TeV_CTau0p1cm_step3-401494f1e1638aa51280a90edc94ebc0/USER', '0.00387135', '1', '0.780865729', 'isGMSB'],
             ['/GMSB_L300TeV_CTau10cm_step0/soffi-GMSB_L300TeV_CTau10cm_step3-e18eec80fff64b1d3595d955b2a6af85/USER', '0.0038706', '1', '0.780865788', 'isGMSB'],
+            ['/GMSB_L300TeV_CTau600cm_step0/soffi-GMSB_L300TeV_CTau600cm_step3-e18eec80fff64b1d3595d955b2a6af85/USER', '0.00388035', '1', '0.780865729', 'isGMSB'],
+            ['/GMSB_L300TeV_CTau1200cm_step0/soffi-GMSB_L300TeV_CTau1200cm_step3-c348d322117ba5f6f60a856979fd0e14/USER', '0.0038618', '1', '0.780865729', 'isGMSB'],
+
             ['/GMSB_L350TeV_CTau0p1cm_step0/soffi-GMSB_L350TeV_CTau0p1cm_step3-7b4951d75b0cdfce8b97c445b952682b/USER', '0.00161162', '1', '0.773663223', 'isGMSB'],
             ['/GMSB_L350TeV_CTau10cm_step0/soffi-GMSB_L350TeV_CTau10cm_step3-7c673dc440a3408d9f3a1cc7f05303ae/USER', '0.00160427', '1', '0.773663163', 'isGMSB'],
+            ['/GMSB_L350TeV_CTau600cm_step0/soffi-GMSB_L350TeV_CTau600cm_step3-7c673dc440a3408d9f3a1cc7f05303ae/USER', '0.00161068', '1', '0.773663223', 'isGMSB'],
+            ['/GMSB_L350TeV_CTau1200cm_step0/soffi-GMSB_L350TeV_CTau1200cm_step3-20e0fe7c912eb4ba0a9d90a66a270bb4/USER', '0.00160865', '1', '0.773663223', 'isGMSB'],
+
             ['/GMSB_L400TeV_CTau0p1cm_step0/soffi-GMSB_L400TeV_CTau0p1cm_step3-401494f1e1638aa51280a90edc94ebc0/USER', '0.00073187', '1', '0.768983364', 'isGMSB'],
             ['/GMSB_L400TeV_CTau10cm_step0/soffi-GMSB_L400TeV_CTau10cm_step3-cea60c68d10a6972420825d5e6a4c6f4/USER', '0.000731975', '1', '0.768983364', 'isGMSB'],
+            ['/GMSB_L400TeV_CTau600cm_step0/soffi-GMSB_L400TeV_CTau600cm_step3-cea60c68d10a6972420825d5e6a4c6f4/USER', '0.000728273', '1', '0.768983364', 'isGMSB'],
+            ['/GMSB_L400TeV_CTau1200cm_step0/soffi-GMSB_L400TeV_CTau1200cm_step3-f3bb7113b9142d3a7446a16dcc0fa766/USER', '0.000731161', '1', '0.768983305', 'isGMSB'],
 
             ]
  
