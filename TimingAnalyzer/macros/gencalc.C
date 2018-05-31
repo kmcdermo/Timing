d10aa73f81063610b2398e51ac29fd8ada37c793
Kevin <kpm82@cornell.edu>
2018-05-31 21:43:01 +0200

    update calc

--- TimingAnalyzer/macros/gencalc.C
+++ TimingAnalyzer/macros/gencalc.C
@@ -1,142 +1,100 @@
-namespace Config
-{
-  static const Int_t   ipho = 0;
-  static const Int_t   npho = ipho+1;
-  static const Float_t ptcut = 0.f;
-  static const Float_t sol  = 2.99792458e10; // cm/s
-  static const Float_t ns_per_s = 1e9; 
-  static const Float_t cm_per_m = 1e2; 
-};
-
-inline Float_t rad2 (const Float_t x, const Float_t y, const Float_t z = 0.f){return x*x + y*y + z*z;}
-inline Float_t hypo (const Float_t x, const Float_t y, const Float_t z = 0.f){return std::sqrt(rad2(x,y,z));}
-inline Float_t theta(const Float_t eta){return 2.f*std::atan(std::exp(-eta));}
-inline Float_t slope_of_eta(const Float_t eta){return std::tan(theta(eta));}
-inline Float_t r_of_eta(const Float_t eta, const Float_t z){return z * slope_of_eta(eta);}
-inline Float_t z_of_eta(const Float_t eta, const Float_t r){return r / slope_of_eta(eta);}
-inline Float_t gamma(const Float_t p, const Float_t m){return std::sqrt(1.f+std::pow(p/m,2));}
-inline Float_t betag(const Float_t p, const Float_t m){return std::abs(p/m);}
-inline Float_t tau  (const Float_t ctau, const Float_t gamma){return ctau * gamma / Config::sol;}
-
-namespace ECAL
-{
-  static const Float_t etaEB    = 1.4442;
-  static const Float_t etaEEmin = 1.566;
-  static const Float_t etaEEmax = 2.5;
-
-  static const Float_t rEB = 129.f; // 1.29 m
-  static const Float_t zEB = z_of_eta(ECAL::etaEB,ECAL::rEB);
-
-  static const Float_t zEE    = 314.f; // 3.14 m 
-  static const Float_t rEEmin = r_of_eta(ECAL::etaEEmax,ECAL::zEE);
-  static const Float_t rEEmax = r_of_eta(ECAL::etaEEmin,ECAL::zEE);
-};
-
-Float_t GetGenPhotonArrivalTime(const Float_t r0, const Float_t z0, const Float_t slope, const Float_t tau0, Float_t& arrival)
-{
-  Float_t time = -1.f;
-  
-  const Float_t z = z0 + ( (ECAL::rEB - r0) / slope );
-  const Float_t r = r0 + ( slope * (ECAL::zEE - z0) );
-  if (std::abs(z) < ECAL::zEB) 
-  {
-    time = (hypo(ECAL::rEB-r0,z-z0) / Config::sol) - (hypo(ECAL::rEB,z) / Config::sol) + tau0;
-    arrival = 0.5;
-  }
-  else if (r > ECAL::rEEmin && r < ECAL::rEEmax)
-  {
-    time = (hypo(r-r0,ECAL::zEE-z0) / Config::sol) - (hypo(r0,ECAL::zEE) / Config::sol) + tau0; 
-    arrival = 1.5;
-  }
-  else
-  {
-    arrival = 2.5;
-  }
-
-  return time;
-}
+#include "gencalc.hh"
 
 void gencalc()
 {
+  // output stuff
+  const TString outname = "sample";
+  TFile * outfile = TFile::Open(outname.Data(),"RECREATE");
+
   // Get Input
   TFile * file = TFile::Open("dispho.root");
   TTree * tree = (TTree*)file->Get("tree/disphotree");
 
+  // start setting variable
+  Int_t       nNeutoPhGr = 0; 
+  TBranch * b_nNeutoPhGr = 0; 
+  TString   s_nNeutoPhGr = "nNeutoPhGr";
+  tree->SetBranchAddress(Form("%s",s_nNeutoPhGr.Data()), &nNeutoPhGr, &b_nNeutoPhGr);
+
   // Set branches
-  Int_t   nNeutoPhGr  = 0; TBranch * b_nNeutoPhGr  = 0; tree->SetBranchAddress(     "nNeutoPhGr"                  , &nNeutoPhGr , &b_nNeutoPhGr);
-  Float_t genNmass    = 0; TBranch * b_genNmass    = 0; tree->SetBranchAddress(Form("genNmass_%i"   ,Config::ipho), &genNmass   , &b_genNmass);
-  Float_t genNE       = 0; TBranch * b_genNE       = 0; tree->SetBranchAddress(Form("genNE_%i"      ,Config::ipho), &genNE      , &b_genNE);
-  Float_t genNpt      = 0; TBranch * b_genNpt      = 0; tree->SetBranchAddress(Form("genNpt_%i"     ,Config::ipho), &genNpt     , &b_genNpt);
-  Float_t genNphi     = 0; TBranch * b_genNphi     = 0; tree->SetBranchAddress(Form("genNphi_%i"    ,Config::ipho), &genNphi    , &b_genNphi);
-  Float_t genNeta     = 0; TBranch * b_genNeta     = 0; tree->SetBranchAddress(Form("genNeta_%i"    ,Config::ipho), &genNeta    , &b_genNeta);
-  Float_t genNprodvx  = 0; TBranch * b_genNprodvx  = 0; tree->SetBranchAddress(Form("genNprodvx_%i" ,Config::ipho), &genNprodvx , &b_genNprodvx);
-  Float_t genNprodvy  = 0; TBranch * b_genNprodvy  = 0; tree->SetBranchAddress(Form("genNprodvy_%i" ,Config::ipho), &genNprodvy , &b_genNprodvy);
-  Float_t genNprodvz  = 0; TBranch * b_genNprodvz  = 0; tree->SetBranchAddress(Form("genNprodvz_%i" ,Config::ipho), &genNprodvz , &b_genNprodvz);
-  Float_t genNdecayvx = 0; TBranch * b_genNdecayvx = 0; tree->SetBranchAddress(Form("genNdecayvx_%i",Config::ipho), &genNdecayvx, &b_genNdecayvx);
-  Float_t genNdecayvy = 0; TBranch * b_genNdecayvy = 0; tree->SetBranchAddress(Form("genNdecayvy_%i",Config::ipho), &genNdecayvy, &b_genNdecayvy);
-  Float_t genNdecayvz = 0; TBranch * b_genNdecayvz = 0; tree->SetBranchAddress(Form("genNdecayvz_%i",Config::ipho), &genNdecayvz, &b_genNdecayvz);
-  Float_t genphE      = 0; TBranch * b_genphE      = 0; tree->SetBranchAddress(Form("genphE_%i"     ,Config::ipho), &genphE     , &b_genphE);
-  Float_t genphpt     = 0; TBranch * b_genphpt     = 0; tree->SetBranchAddress(Form("genphpt_%i"    ,Config::ipho), &genphpt    , &b_genphpt);
-  Float_t genphphi    = 0; TBranch * b_genphphi    = 0; tree->SetBranchAddress(Form("genphphi_%i"   ,Config::ipho), &genphphi   , &b_genphphi);
-  Float_t genpheta    = 0; TBranch * b_genpheta    = 0; tree->SetBranchAddress(Form("genpheta_%i"   ,Config::ipho), &genpheta   , &b_genpheta);
+  std::vector<GenStruct> GenInfos(Config::ngen);
+  for (Int_t igen = 0; igen < Config::ngen; igen++)
+  {
+    auto & GenInfo = GenInfos[igen];
+    tree->SetBranchAddress(Form("%s_%i",GenInfo.s_genNmass.Data(),igen), &GenInfo.genNmass, &GenInfo.b_genNmass);
+    tree->SetBranchAddress(Form("%s_%i",GenInfo.s_genNE.Data(),igen), &GenInfo.genNE, &GenInfo.b_genNE);
+    tree->SetBranchAddress(Form("%s_%i",GenInfo.s_genNpt.Data(),igen), &GenInfo.genNpt, &GenInfo.b_genNpt);
+    tree->SetBranchAddress(Form("%s_%i",GenInfo.s_genNphi.Data(),igen), &GenInfo.genNphi, &GenInfo.b_genNphi);
+    tree->SetBranchAddress(Form("%s_%i",GenInfo.s_genNeta.Data(),igen), &GenInfo.genNeta, &GenInfo.b_genNeta);
+    tree->SetBranchAddress(Form("%s_%i",GenInfo.s_genNprodvx.Data(),igen), &GenInfo.genNprodvx, &GenInfo.b_genNprodvx);
+    tree->SetBranchAddress(Form("%s_%i",GenInfo.s_genNprodvy.Data(),igen), &GenInfo.genNprodvy, &GenInfo.b_genNprodvy);
+    tree->SetBranchAddress(Form("%s_%i",GenInfo.s_genNprodvz.Data(),igen), &GenInfo.genNprodvz, &GenInfo.b_genNprodvz);
+    tree->SetBranchAddress(Form("%s_%i",GenInfo.s_genNdecayvx.Data(),igen), &GenInfo.genNdecayvx, &GenInfo.b_genNdecayvx);
+    tree->SetBranchAddress(Form("%s_%i",GenInfo.s_genNdecayvy.Data(),igen), &GenInfo.genNdecayvy, &GenInfo.b_genNdecayvy);
+    tree->SetBranchAddress(Form("%s_%i",GenInfo.s_genNdecayvz.Data(),igen), &GenInfo.genNdecayvz, &GenInfo.b_genNdecayvz);
+    tree->SetBranchAddress(Form("%s_%i",GenInfo.s_genphE.Data(),igen), &GenInfo.genphE, &GenInfo.b_genphE);
+    tree->SetBranchAddress(Form("%s_%i",GenInfo.s_genphpt.Data(),igen), &GenInfo.genphpt, &GenInfo.b_genphpt);
+    tree->SetBranchAddress(Form("%s_%i",GenInfo.s_genphphi.Data(),igen), &GenInfo.genphphi, &GenInfo.b_genphphi);
+    tree->SetBranchAddress(Form("%s_%i",GenInfo.s_genpheta.Data(),igen), &GenInfo.genpheta, &GenInfo.b_genpheta);
+  }
 
   // Set hists
-  TH1F * ctau = new TH1F("ctau","ctau",100,0,2);
+  TH1F * ctau = new TH1F("ctau","Neutralino Travel Distance (c#tau);c#tau (#tilde{#chi}^{1}_{0} Rest Frame) [cm];nNeutralinos",100,0,2);
   ctau->Sumw2();
-  TH1F * time = new TH1F("arrival_time","arrival time [ns]",100,0,1000);
+  TH2F * decay2D = new TH2F("decay2D","Neutralino Decay Vertex;Radius (Lab Frame) [cm];Z (Lab Frame) [cm];nNeutralinos",1000,0,1000,1000,0,1000);
+  decay2D->Sumw2();
+  TH1F * time = new TH1F("arrival_time","Photon Arrival Time in ECAL;Time (Lab Frame) [ns];nPhotons",100,0,1000);
   time->Sumw2();
-  TH1F * det = new TH1F("detarrival","detector arrival",3,0,3);
+  TH1F * det = new TH1F("detarrival","Photon ECAL Arrival;;nNeutralinos",3,0,3);
   det->Sumw2();
 
   std::vector<TString> labels = {"EB","EE","Missed"};
   for (int ibin = 1; ibin <= det->GetXaxis()->GetNbins(); ibin++){det->GetXaxis()->SetBinLabel(ibin,labels[ibin-1]);}
 
   const UInt_t entries = tree->GetEntries();
-  std::cout << "Entries: " << entries << std::endl;
   for (UInt_t entry = 0; entry < entries; entry++)
   {
     b_nNeutoPhGr ->GetEntry(entry);
-    b_genNmass   ->GetEntry(entry);
-    b_genNE      ->GetEntry(entry);
-    b_genNpt     ->GetEntry(entry);
-    b_genNeta    ->GetEntry(entry);
-    b_genNphi    ->GetEntry(entry);
-    b_genNprodvx ->GetEntry(entry);
-    b_genNprodvy ->GetEntry(entry);
-    b_genNprodvz ->GetEntry(entry);
-    b_genNdecayvx->GetEntry(entry);
-    b_genNdecayvy->GetEntry(entry);
-    b_genNdecayvz->GetEntry(entry);
-    b_genphE     ->GetEntry(entry);
-    b_genphpt    ->GetEntry(entry);
-    b_genpheta   ->GetEntry(entry);
-    b_genphphi   ->GetEntry(entry);
 
-    if (nNeutoPhGr < Config::npho) continue;
-    if (genphpt < Config::ptcut) continue;
+    for (Int_t igen = 0; igen < nNeutoPhGr; igen++)
+    {
+      auto & GenInfo = GenInfos[igen];
 
-    TLorentzVector genN_lorvec; genN_lorvec.SetPtEtaPhiE(genNpt,genNeta,genNphi,genNE);
-    const Float_t genNp       = hypo(genN_lorvec.Px(),genN_lorvec.Py(),genN_lorvec.Pz());
-    const Float_t genNbg      = betag(genNp,genNmass);
-    const Float_t genNtraveld = hypo(genNdecayvx-genNprodvx,genNdecayvy-genNprodvy,genNdecayvz-genNprodvz);
-    const Float_t genNctau    = genNtraveld/genNbg; 
-    const Float_t genNgamma   = gamma(genNp,genNmass);
-    const Float_t genNdecayvr = hypo(genNdecayvx,genNdecayvy);
-    const Float_t genphslope  = slope_of_eta(genpheta);
-    const Float_t genNlabtime = tau(genNctau,genNgamma);
+      GenInfo.b_genNmass   ->GetEntry(entry);
+      GenInfo.b_genNE      ->GetEntry(entry);
+      GenInfo.b_genNpt     ->GetEntry(entry);
+      GenInfo.b_genNeta    ->GetEntry(entry);
+      GenInfo.b_genNphi    ->GetEntry(entry);
+      GenInfo.b_genNprodvx ->GetEntry(entry);
+      GenInfo.b_genNprodvy ->GetEntry(entry);
+      GenInfo.b_genNprodvz ->GetEntry(entry);
+      GenInfo.b_genNdecayvx->GetEntry(entry);
+      GenInfo.b_genNdecayvy->GetEntry(entry);
+      GenInfo.b_genNdecayvz->GetEntry(entry);
+      GenInfo.b_genphE     ->GetEntry(entry);
+      GenInfo.b_genphpt    ->GetEntry(entry);
+      GenInfo.b_genpheta   ->GetEntry(entry);
+      GenInfo.b_genphphi   ->GetEntry(entry);
+
+      TLorentzVector genN_lorvec; genN_lorvec.SetPtEtaPhiE(GenInfo.genNpt,GenInfo.genNeta,GenInfo.genNphi,GenInfo.genNE);
+      const Float_t genNp       = hypo(genN_lorvec.Px(),genN_lorvec.Py(),genN_lorvec.Pz());
+      const Float_t genNbg      = betag(genNp,GenInfo.genNmass);
+      const Float_t genNtraveld = hypo(GenInfo.genNdecayvx-GenInfo.genNprodvx,GenInfo.genNdecayvy-GenInfo.genNprodvy,GenInfo.genNdecayvz-GenInfo.genNprodvz);
+      const Float_t genNctau    = genNtraveld/genNbg;
+      const Float_t genNgamma   = gamma(genNp,GenInfo.genNmass);
+      const Float_t genNdecayvr = hypo(GenInfo.genNdecayvx,GenInfo.genNdecayvy);
+      const Float_t genphslope  = slope_of_eta(GenInfo.genpheta);
+      const Float_t genNlabtime = tau(genNctau,genNgamma);
     
-    Float_t arrival = -1.f;
-    const Float_t genphtime   = GetGenPhotonArrivalTime(genNdecayvr,genNdecayvz,genphslope,genNlabtime,arrival);
+      Float_t detector = -1.f;
+      const Float_t genphtime   = GetGenPhotonArrivalTime(genNdecayvr,GenInfo.genNdecayvz,genphslope,genNlabtime,detector);
 
-    ctau->Fill(genNctau);
-    time->Fill(genphtime*Config::ns_per_s);
-    det->Fill(arrival);
-  }
-
-  for (int ibin = 1; ibin <= det->GetXaxis()->GetNbins(); ibin++)
-  {
-    std::cout << labels[ibin-1].Data() << " " << det->GetBinContent(ibin) << std::endl;
-  }
+      ctau->Fill(genNctau);
+      time->Fill(genphtime*Config::ns_per_s);
+      det->Fill(detecor);
+      decay2D->Fill(genNdecayvr,GenInfo.genNdecayvz);
+    } // end loop neutralinos
+  } // end loop entries
 
   TFormula form("texp","[0]*exp(-x/[1])");
   TF1 * fit = new TF1("texp_fit",form.GetName(),0.f,100.f);
@@ -150,4 +108,34 @@ void gencalc()
   ctau->Draw();
   fit->Draw("same");
   canv->SaveAs("ctau.png");
+
+  outfile->cd();
+  ctau->Write();
+  time->Write();
+  det->Write();
+  decay2D->Write():
+}
+
+Float_t GetGenPhotonArrivalTime(const Float_t r0, const Float_t z0, const Float_t slope, const Float_t tau0, Float_t& detector)
+{
+  Float_t time = -1.f;
+  
+  const Float_t z = z0 + ( (ECAL::rEB - r0) / slope );
+  const Float_t r = r0 + ( slope * (ECAL::zEE - z0) );
+  if (std::abs(z) < ECAL::zEB) 
+  {
+    time = (hypo(ECAL::rEB-r0,z-z0) / Config::sol) - (hypo(ECAL::rEB,z) / Config::sol) + tau0;
+    detector = 0.5;
+  }
+  else if (r > ECAL::rEEmin && r < ECAL::rEEmax)
+  {
+    time = (hypo(r-r0,ECAL::zEE-z0) / Config::sol) - (hypo(r0,ECAL::zEE) / Config::sol) + tau0; 
+    detector = 1.5;
+  }
+  else
+  {
+    detector = 2.5;
+  }
+
+  return time;
 }
