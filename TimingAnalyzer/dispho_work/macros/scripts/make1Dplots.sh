ddf783c2eaccd34bb33aeb025bf14ef913a360d3
Kevin <kpm82@cornell.edu>
2018-06-07 18:03:36 +0200

    add insel dir

--- TimingAnalyzer/dispho_work/macros/scripts/make1Dplots.sh
+++ TimingAnalyzer/dispho_work/macros/scripts/make1Dplots.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 outdir=${1:-"plots/ntuples_v4/checks_v2"}
+insel_dir=""
 insel="always_true"
 
 for inputgroup in "qcd signals_qcd control_qcd SinglePhoton" "gjets signals_gjets control_gjets DoubleEG" "sr signals_sr signal SinglePhoton"
@@ -12,7 +13,7 @@ do echo ${inputgroup} | while read -r infile insignalfile sel pdname
 	    then
 		plot=${line}
 		misc="misc"
-		./scripts/runTreePlotter.sh "skims/${infile}.root" "skims/${insignalfile}.root" "cut_config/${insel}.txt" "plot_config/${plot}.txt" "misc_config/${misc}.txt" "${plot}_${sel}_${pdname}" "${outdir}/${sel}"
+		./scripts/runTreePlotter.sh "skims/${infile}.root" "skims/${insignalfile}.root" "cut_config/${insel_dir}${insel}.txt" "plot_config/${plot}.txt" "misc_config/${misc}.txt" "${plot}_${sel}_${pdname}" "${outdir}/${sel}_${insel}"
 	    fi
 	done < scripts/standard_plots.txt
     done
@@ -24,7 +25,7 @@ do echo ${inputgroup} | while read -r infile insignalfile sel pdname
 	for plot in phoseedtime_0 met met_zoom
 	do
 	    misc="misc"
-	    ./scripts/runTreePlotter.sh "skims/${infile}.root" "skims/${insignalfile}.root" "cut_config/${insel}.txt" "plot_config/${plot}.txt" "misc_config/${misc}.txt" "${plot}_${sel}_${pdname}" "${outdir}/${sel}"
+	    ./scripts/runTreePlotter.sh "skims/${infile}.root" "skims/${insignalfile}.root" "cut_config/${insel_dir}${insel}.txt" "plot_config/${plot}.txt" "misc_config/${misc}.txt" "${plot}_${sel}_${pdname}" "${outdir}/${sel}_${insel}"
 	done
     done
 done
@@ -35,7 +36,7 @@ do echo ${inputgroup} | while read -r infile insignalfile sel pdname
 	for plot in phoseedtime_0 met met_zoom
 	do
 	    misc="misc_blind"
-	    ./scripts/runTreePlotter.sh "skims/${infile}.root" "skims/${insignalfile}.root" "cut_config/${insel}.txt" "plot_config/${plot}.txt" "misc_config/${misc}.txt" "${plot}_${sel}_${pdname}" "${outdir}/${sel}"
+	    ./scripts/runTreePlotter.sh "skims/${infile}.root" "skims/${insignalfile}.root" "cut_config/${insel_dir}${insel}.txt" "plot_config/${plot}.txt" "misc_config/${misc}.txt" "${plot}_${sel}_${pdname}" "${outdir}/${sel}_${insel}"
 	done
     done
 done
