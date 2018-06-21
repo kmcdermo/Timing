c23f3abbe4e065b0627840221f80fd329af16205
Kevin McDermott <kpm82@cornell.edu>
2018-06-21 18:20:17 +0200

    more grid points:

--- GEN_SIM/GMSB/autogenGMSBgrid.sh
+++ GEN_SIM/GMSB/autogenGMSBgrid.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-for lambda in 100 150 200 250 300 350 400
+for lambda in 100 150 200 250 300 350 400 500 600
 do
-    for ctau in 0.1 10 200 400 600 800 1000 1200
+    for ctau in 0.001 0.01 0.1 1 10 100 200 400 600 800 1000 1200 5000 10000
     do
 	sed "s/XXX/${lambda}/" <GMSB_LXXXTeV_CtauYYYcm_Pythia8_13TeV_cff.py >GMSB_L${lambda}TeV_Ctau${ctau}cm_Pythia8_13TeV_cff.py
 	sed -i '' "s/YYY/${ctau}/" GMSB_L${lambda}TeV_Ctau${ctau}cm_Pythia8_13TeV_cff.py
