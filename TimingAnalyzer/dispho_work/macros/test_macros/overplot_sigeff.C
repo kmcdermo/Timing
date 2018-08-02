0125fc0446b727971297771d353f1f0e9debc2aa
Kevin <kpm82@cornell.edu>
2018-08-02 18:47:27 +0200

    update overplotter for sigeff

--- TimingAnalyzer/dispho_work/macros/test_macros/overplot_sigeff.C
+++ TimingAnalyzer/dispho_work/macros/test_macros/overplot_sigeff.C
@@ -1,6 +1,8 @@
 #include "Common.cpp+"
 
 #include "TStyle.h"
+#include "TMarker.h"
+#include "TColor.h"
 #include "TFile.h"
 #include "TGraphAsymmErrors.h"
 #include "TCanvas.h"
@@ -11,41 +13,52 @@
 struct File
 {
   File() {}
-  File(const TString & label, const TString & filename) 
-    : label(label), filename(filename) {}
+  File(const TString & label, const TString & filename, const Color_t & color)
+    : label(label), filename(filename), color(color) {}
   
   TString label;
   TString filename;
+  Color_t color;
   TFile * file;
 };
 
 struct Graph
 {
   Graph() {}
-  Graph(const TString & label, const TString & graphname)
-    : label(label), graphname(graphname) {}
+  Graph(const TString & label, const TString & graphname, const Marker_t & marker)
+    : label(label), graphname(graphname), marker(marker) {}
   
   TString label;
   TString graphname;
+  Marker_t marker;
 };
 
 struct SigEff
 {
   SigEff() {}
-  SigEff(const TString & label, const TGraphAsymmErrors *& graph) 
-    : label(label), graph(graph) {}
+  SigEff(const TString & label, TGraphAsymmErrors * graph, const Color_t & color, const Marker_t & marker)
+    : label(label), graph(graph), color(color), marker(marker) {}
   
   TString label;
   TGraphAsymmErrors * graph;
+  Color_t color;
+  Marker_t marker;
 };
 
 void overplot_sigeff()
 {
+  // set style
+  auto tdrStyle = new TStyle("TDRStyle","Style for P-TDR");
+  Common::SetTDRStyle(tdrStyle);
+
   // set file names
-  std::vector<File> files = {{"Orig","orig_sig_eff.root"},{"DEG","deg_sig_eff.root"},{"SPH200","sph200_sig_eff.root"}};
+  std::vector<File> files = {{"Orig","orig_sig_eff.root",Common::ColorVec[0]},
+			     {"DEG","deg_sig_eff.root",Common::ColorVec[1]},
+			     {"SPH200","sph200_sig_eff.root",Common::ColorVec[2]}};
 
   // set up graph names
-  std::vector<Graph> graphs = {{"0.1","GMSB_CTau0p1cm_Graph"},{"1200","GMSB_CTau1200cm_Graph"}}; 
+  std::vector<Graph> graphs = {{"0.1","GMSB_CTau0p1cm_Graph",kFullCircle},
+			       {"1200","GMSB_CTau1200cm_Graph",kFullSquare}};
 
   // read in files, graphs
   std::vector<SigEff> sigeffs;
@@ -54,7 +67,7 @@ void overplot_sigeff()
     file.file = TFile::Open(file.filename.Data());
     for (auto & graph : graphs)
     {
-      sigeffs.emplace_back(file.label+", c#tau: "+graph.label+"cm",(TGraphAsymmErrors*)file.file->Get(graph.graphname.Data()));
+      sigeffs.emplace_back(file.label+", c#tau: "+graph.label+" cm",(TGraphAsymmErrors*)file.file->Get(graph.graphname.Data()),file.color,graph.marker);
     }
   }
     
@@ -64,7 +77,7 @@ void overplot_sigeff()
   canv->SetLogy(1);
 
   // new the legend
-  auto leg = new TLegend(0.2,0.75,0.4,0.92);
+  auto leg = new TLegend(0.55,0.2,0.82,0.42);
   leg->SetBorderSize(1);
   leg->SetLineColor(kBlack);
 
@@ -72,9 +85,13 @@ void overplot_sigeff()
   for (auto i = 0U; i < sigeffs.size(); i++)
   {
     auto & sigeff = sigeffs[i];
-    
+
+    sigeff.graph->SetLineColor(sigeff.color);
+    sigeff.graph->SetMarkerColor(sigeff.color);
+    sigeff.graph->SetMarkerStyle(sigeff.marker);
     sigeff.graph->GetHistogram()->GetYaxis()->SetRangeUser(1e-3f,1.f);
-    sigeff.graph->Draw(i>0?"P SAME":"AP");
+
+    sigeff.graph->Draw(i>0?"PZ SAME":"APZ");
     leg->AddEntry(sigeff.graph,sigeff.label.Data(),"LEP");
   }
 
@@ -89,4 +106,6 @@ void overplot_sigeff()
 
   for (auto & sigeff : sigeffs) delete sigeff.graph;
   for (auto & file : files) delete file.file;
+  
+  delete tdrStyle;
 }
