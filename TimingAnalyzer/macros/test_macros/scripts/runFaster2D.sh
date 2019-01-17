aae39e6dd7dbc0f34ce1a0bb76cbeb04ca13a987
Kevin <kpm82@cornell.edu>
2019-01-17 03:11:56 +0100

    use poisson

--- TimingAnalyzer/macros/test_macros/scripts/runFaster2D.sh
+++ TimingAnalyzer/macros/test_macros/scripts/runFaster2D.sh
@@ -5,16 +5,17 @@ dir="dispho/plots/madv2_v3/test/ABCD"
 outdir="${basedir}/${dir}"
 mkdir -p "${outdir}"
 
-gjets_data="skims/v3/orig_2phosCR/gjets.root Data_Tree NONE Data_GJetsCR"
-qcd_data="skims/v3/orig_2phosCR/qcd.root Data_Tree NONE Data_QCDCR"
-declare -a inputs=(gjets_data qcd_data)
+gjets="skims/v3/orig_2phosCR/gjets.root Data_Tree NONE GJets"
+qcd="skims/v3/orig_2phosCR/qcd.root Data_Tree NONE QCD"
+dyll="skims/v3/orig_2phosCR/dyll.root Data_Tree NONE DYLL"
+declare -a inputs=(gjets qcd dyll)
 
 declare -a time_bins=(1 2 3)
 declare -a met_bins=(50 75 100 150 200)
 
 textfile="MET_v_Time.csv"
 > ${textfile}
-echo "Sample,Time,MET,obsA,obsB,obsC,obsD,c1{obsB/obsA},c2{obsD/obsA},predC{obsA*c1*c2},pullObsC{(obsC-predC)/obsCunc},pullPredC{(obsC-predC)/predCunc},pullC{(obsC-predC)/sqrt(obsCunc^2+predCunc^2)}" >> ${textfile}
+echo "Control Region [Data],Time Boundary [ns],MET Boundary [GeV],obsA,obsB,obsC,obsD,c1{obsB/obsA},c2{obsD/obsA},predC{obsA*c1*c2},pullObsCLow{(obsC-predC)/obsCuncLow},pullObsCUp{(obsC-predC)/obsCuncUp},pullPredCLow{(obsC-predC)/predCuncLow},pullPredCUp{(obsC-predC)/predCuncUp},pullCLow{(obsC-predC)/sqrt(obsCuncLow^2+predCuncLow^2)},pullCUp{(obsC-predC)/sqrt(obsCuncUp^2+predCuncUp^2)}" >> ${textfile}
 
 for time_bin in "${time_bins[@]}"
 do
