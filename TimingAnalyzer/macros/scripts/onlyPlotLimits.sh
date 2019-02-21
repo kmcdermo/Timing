b053fb58d5a8ae346cf7a93f67df3f9f81b8ffde
Kevin <kpm82@cornell.edu>
2019-02-21 23:06:58 +0100

    only plot limits

---
+++ TimingAnalyzer/macros/scripts/onlyPlotLimits.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+##################
+## Source First ##
+##################
+
+source scripts/common_variables.sh
+
+###################
+## Configuration ##
+###################
+
+## Command Line Input
+outdir=${1:-"madv2_v3/full_chain/results_ABCD"}
+use_obs=${2:-"false"}
+save_meta_data=${3:-0}
+
+## Combine config
+outcombname="AsymLim"
+outlimitdir="output"
+
+## Limit plot config
+limit="limits2D"
+outlimit1D="limit1D"
+outlimit2D="limit2D"
+outlimitplotdir="limits" # for www
+
+## derived input
+if [[ "use_obs" == "true" ]]
+then
+    doobs=1
+else
+    doobs=0
+fi
+
+#########################
+## Make 1D Limit Plots ##
+#########################
+
+echo "Running limits 1D"
+./scripts/runLimits1D.sh "${outlimitdir}" "${outcombname}" ${doobs} "${MainEra}" "${outlimit1D}" "${outdir}/${outlimitplotdir}"
+
+#########################
+## Make 2D Limit Plots ##
+#########################
+
+echo "Running limits 2D"
+./scripts/runLimits2D.sh "${outlimitdir}" "${outcombname}" ${doobs} "${limitconfigdir}/${limit}.${inTextExt}" "${MainEra}" ${save_meta_data} "${outlimit2D}" "${outdir}/${outlimitplotdir}"
+
+###################
+## Final Message ##
+###################
+
+echo "Finished only making limit plots (ABCD)"
