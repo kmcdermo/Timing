be4dcfbe4d97856a602af4f171b9427a8987cabe
Kevin <kpm82@cornell.edu>
2019-05-09 02:13:17 +0200

    update diagnostics

--- TimingAnalyzer/macros/test_macros/printDiagnostics.C
+++ TimingAnalyzer/macros/test_macros/printDiagnostics.C
@@ -15,45 +15,8 @@ struct Var
   Double_t unc;
 };
 
-void prepVars(TFile * file, std::map<Param,Var> & varMap, std::map<std::pair<Param,Param>,Double_t> & covMap)
-{
-  const auto & fit_b = (RooFitResult*)file->Get("fit_b");
-  const auto & pars_list = fit_b->floatParsFinal();
-
-  for (const auto & paramPair : paramMap)
-  {
-    const auto param = paramPair.first;
-    const auto & param_name = paramPair.second;
-    const auto idx = pars_list.index(param_name.Data());
-    const auto var = (RooRealVar*)pars_list.at(idx);
-
-    varMap[param].val = var->getVal();
-    varMap[param].unc = var->getError();
-    //    std::cout << param_name.Data() << ": " << varMap.at(param).val << " +/- " << varMap.at(param).unc << std::endl;
-  }
-  
-  for (const auto & paramPair_i : paramMap)
-  {
-    const auto param1 = paramPair_i.first;
-    const auto & param_name1 = paramPair_i.second;
-    
-    for (const auto & paramPair_j : paramMap)
-    {
-      const auto param2 = paramPair_j.first;
-      const auto & param_name2 = paramPair_j.second;
-      covMap[{param1,param2}] = fit_b->correlation(param_name1,param_name2)*varMap[param1].unc*varMap[param2].unc;
-      //      std::cout << param_name1.Data() << " " << param_name2.Data() << " : " << fit_b->correlation(param_name1,param_name2) << " = " << covMap.at({param1,param2}) << std::endl;
-    }
-  }
-
-  // const auto & covMat = fit_b->covarianceMatrix();
-  // for (auto i = 0; i < covMat.GetNcols(); i++)
-  //   for (auto j = 0; j < covMat.GetNrows(); j++)
-  //     std::cout << i << "," << j << " : " << covMat[i][j] << std::endl;
-
-  // delete it all
-  delete fit_b;
-}
+void dumpBackgrounds(const TString & fitname, const TString & fitlabel, TFile * file_tot, TFile * file_stat, std::ofstream & output);
+void prepVars(const TString & fitname, TFile * file, std::map<Param,Var> & varMap, std::map<std::pair<Param,Param>,Double_t> & covMap);
 
 void printDiagnostics(const TString & filename_stat = "", const TString & filename_tot = "", const TString & label = "")
 {
@@ -61,6 +24,23 @@ void printDiagnostics(const TString & filename_stat = "", const TString & filena
   auto file_stat = TFile::Open(filename_stat.Data());
   auto file_tot  = TFile::Open(filename_tot .Data());
 
+  // output file
+  std::ofstream output(label+".txt",std::ios::trunc);
+  output << "bin: val +/- stat +/- syst" << std::endl << std::endl;
+
+  // do bkgd-only
+  dumpBackgrounds("fit_b","Background Only",file_stat,file_tot,output);
+  
+  // do bkgd+sign
+  dumpBackgrounds("fit_s","Background+Signal",file_stat,file_tot,output);
+
+  // delete it all
+  delete file_tot;
+  delete file_stat;
+}
+
+void dumpBackgrounds(const TString & fitname, const TString & fitlabel, TFile * file_tot, TFile * file_stat, std::ofstream & output)
+{
   std::map<Param,Var> varMap_stat;
   std::map<Param,Var> varMap_tot;
 
@@ -68,8 +48,8 @@ void printDiagnostics(const TString & filename_stat = "", const TString & filena
   std::map<std::pair<Param,Param>,Double_t> covMap_tot;
 
   // get vars
-  prepVars(file_stat,varMap_stat,covMap_stat);
-  prepVars(file_tot ,varMap_tot ,covMap_tot);
+  prepVars(fitname,file_stat,varMap_stat,covMap_stat);
+  prepVars(fitname,file_tot ,varMap_tot ,covMap_tot);
 
   // compute yield: A
   const auto predA          = varMap_tot.at(Param::bkg1).val;
@@ -121,15 +101,51 @@ void printDiagnostics(const TString & filename_stat = "", const TString & filena
   const auto predD_unc_syst = std::sqrt(std::pow(predD_unc_tot,2)-std::pow(predD_unc_stat,2));
   
   // print it out
-  std::ofstream output(label+".txt",std::ios::trunc);
-  output << "bin: val +/- stat unc. +/- syst unc. = tot unc." << std::endl;
+  output << fitlabel.Data() << std::endl;
   output << "A: " << predA << " +/- " << predA_unc_stat << " +/- " << predA_unc_syst << " = " << predA_unc_tot << std::endl;
   output << "B: " << predB << " +/- " << predB_unc_stat << " +/- " << predB_unc_syst << " = " << predB_unc_tot << std::endl;
   output << "C: " << predC << " +/- " << predC_unc_stat << " +/- " << predC_unc_syst << " = " << predC_unc_tot << std::endl;
   output << "D: " << predD << " +/- " << predD_unc_stat << " +/- " << predD_unc_syst << " = " << predD_unc_tot << std::endl;
+  output << std::endl;
+}
+
+void prepVars(const TString & fitname, TFile * file, std::map<Param,Var> & varMap, std::map<std::pair<Param,Param>,Double_t> & covMap)
+{
+  const auto & fit = (RooFitResult*)file->Get(fitname.Data());
+  const auto & pars_list = fit->floatParsFinal();
+
+  for (const auto & paramPair : paramMap)
+  {
+    const auto param = paramPair.first;
+    const auto & param_name = paramPair.second;
+    const auto idx = pars_list.index(param_name.Data());
+    const auto var = (RooRealVar*)pars_list.at(idx);
+
+    varMap[param].val = var->getVal();
+    varMap[param].unc = var->getError();
+    //    std::cout << param_name.Data() << ": " << varMap.at(param).val << " +/- " << varMap.at(param).unc << std::endl;
+  }
+  
+  for (const auto & paramPair_i : paramMap)
+  {
+    const auto param1 = paramPair_i.first;
+    const auto & param_name1 = paramPair_i.second;
+    
+    for (const auto & paramPair_j : paramMap)
+    {
+      const auto param2 = paramPair_j.first;
+      const auto & param_name2 = paramPair_j.second;
+      covMap[{param1,param2}] = fit->correlation(param_name1,param_name2)*varMap[param1].unc*varMap[param2].unc;
+      //      std::cout << param_name1.Data() << " " << param_name2.Data() << " : " << fit->correlation(param_name1,param_name2) << " = " << covMap.at({param1,param2}) << std::endl;
+    }
+  }
+
+  // const auto & covMat = fit->covarianceMatrix();
+  // for (auto i = 0; i < covMat.GetNcols(); i++)
+  //   for (auto j = 0; j < covMat.GetNrows(); j++)
+  //     std::cout << i << "," << j << " : " << covMat[i][j] << std::endl;
 
   // delete it all
-  delete file_tot;
-  delete file_stat;
+  delete fit;
 }
 
